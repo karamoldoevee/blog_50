8983d9e322c3b96190df5977627a001f02636b89
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-10-22 17:11:31 +0600

    Созданы функции def_clean_title и def_clean

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,13 +1,32 @@
 from django import forms
-from webapp.models import Article, Comment, STATUS_ACTIVE, Tag
+from django.core.exceptions import ValidationError
+
+from webapp.models import Article, Comment, STATUS_ACTIVE
 
 
 class ArticleForm(forms.ModelForm):
-    tags = forms.CharField(max_length=31, required=False)
     class Meta:
         model = Article
-        exclude = ['created_at', 'updated_at', 'tags']
+        exclude = ['created_at', 'updated_at', 'status']
 
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        min_length = 10
+        if len(title) < min_length:
+            raise ValidationError(
+                'Title should be at least %(length)s symbols long.',
+                code='title_too_short',
+                params={'length': min_length}
+            )
+        return title.capitalize()
+
+    def clean(self):
+        super().clean()
+        title = self.cleaned_data.get('title', '')
+        text = self.cleaned_data.get('text', '')
+        if title.lower() == text.lower():
+            raise ValidationError('Text should not duplicate title')
+        return self.cleaned_data
 
 
 class CommentForm(forms.ModelForm):
@@ -15,7 +34,6 @@ class CommentForm(forms.ModelForm):
         super().__init__(**kwargs)
         self.fields['article'].queryset = Article.objects.filter(status=STATUS_ACTIVE)
 
-    # article = forms.ModelChoiceField(queryset=Article.objects.filter(status=STATUS_ACTIVE), label='Статья')
 
     class Meta:
         model = Comment
@@ -31,13 +49,15 @@ class ArticleCommentForm(forms.ModelForm):
 class SimpleSearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False, label='Найти')
 
-class FullSearchForm(forms.Form):
-     text = forms.CharField(max_length=100, required=False, label='Текст')
-     in_title = forms.BooleanField(initial=True, required=False, label='В заголовках')
-     in_text = forms.BooleanField(initial=True, required=False, label='В тексте')
-     in_tags = forms.BooleanField(initial=True, required=False, label='В тегах')
-     in_comment_text = forms.BooleanField(initial=False, required=False, label='В тексте комментариев')
 
-     author = forms.CharField(max_length=100, required=False, label='Автор')
-     in_articles = forms.BooleanField(initial=True, required=False, label='Статей')
-     in_commnets = forms.BooleanField(initial=False, required=False, label='Комментариев')
+class FullSearchForm(forms.Form):
+    text = forms.CharField(max_length=100, required=False, label='Текст')
+    in_title = forms.BooleanField(initial=True, required=False, label='В заголовках')
+    in_text = forms.BooleanField(initial=True, required=False, label='В тексте')
+    in_tags = forms.BooleanField(initial=True, required=False, label='В тегах')
+    in_comment_text = forms.BooleanField(initial=False, required=False, label='В тексте комментариев')
+
+    author = forms.CharField(max_length=100, required=False, label='Автор')
+    in_articles = forms.BooleanField(initial=True, required=False, label='Статей')
+    in_comments = forms.BooleanField(initial=False, required=False, label='Комментариев')
+
