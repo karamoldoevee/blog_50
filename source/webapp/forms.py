b00fb0440952092cc5e67b54bdd0ede5e3eca946
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-10-22 16:56:06 +0600

    Добавлен FullSearchForm в forms py

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -31,3 +31,13 @@ class ArticleCommentForm(forms.ModelForm):
 class SimpleSearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False, label='Найти')
 
+class FullSearchForm(forms.Form):
+     text = forms.CharField(max_length=100, required=False, label='Текст')
+     in_title = forms.BooleanField(initial=True, required=False, label='В заголовках')
+     in_text = forms.BooleanField(initial=True, required=False, label='В тексте')
+     in_tags = forms.BooleanField(initial=True, required=False, label='В тегах')
+     in_comment_text = forms.BooleanField(initial=False, required=False, label='В тексте комментариев')
+
+     author = forms.CharField(max_length=100, required=False, label='Автор')
+     in_articles = forms.BooleanField(initial=True, required=False, label='Статей')
+     in_commnets = forms.BooleanField(initial=False, required=False, label='Комментариев')
